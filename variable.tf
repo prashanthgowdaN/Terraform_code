1b67eda963c882be17ee3ad9e56d6ace5c9be490
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-11 13:42:52 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -3,6 +3,4 @@ variable "aws_secret_key" {}
 variable "instance_type" {
   default = "t2.micro"
 }
-variable "ami" {
-  default = "ami-0c55b159cbfafe1f0"  # Replace with an AMI ID from your region
-}
+
