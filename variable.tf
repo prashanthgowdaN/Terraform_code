c9678426d9b0f2dc6575c3367fa02d659e332327
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-12 21:17:50 +0530

    Update variable.tf
    Signed-off-by: prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>

--- variable.tf
+++ variable.tf
@@ -1,6 +1,7 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "instance_type" {
-  default = "t2.micro"
+variable "vm_names" {
+  type    = list(string)
+  default = ["web-server", "db-server", "cache-server"]  # We can use the Host Name and its only for single Image 
 }
 
