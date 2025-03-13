948e5a7a683b6af0b0acf066a61f5aa36dd79c20
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-13 15:45:50 +0530

    Update main.tf
    Signed-off-by: prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "TestVM01" {
   count         = length(var.vm_names)
   ami           = "ami-08b5b3a93ed654d19"
-  instance_type = "t2.micro"
+  instance_type = "t2.medium"
 
   tags = {
     Name = var.vm_names[count.index]
