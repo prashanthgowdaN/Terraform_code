14dc33096dde71df15d0f548d88eeb2252d43f06
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-14 22:11:51 +0530

    Update variable.tf
    Signed-off-by: prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>

--- Modules/variable.tf
+++ Modules/variable.tf
@@ -1,1 +1,26 @@
+variable "ami_id" {
+  description = "The AMI ID to use for the EC2 instance"
+  type        = string
+}
 
+variable "instance_type" {
+  description = "The type of instance to create"
+  type        = string
+  default     = "t2.micro"  # Default instance type
+}
+
+variable "vpc_id" {
+  description = "VPC ID where the instance will be created"
+  type        = string
+}
+
+variable "subnet_id" {
+  description = "The subnet ID where the EC2 instance will be placed"
+  type        = string
+}
+
+variable "disk_size" {
+  description = "Size of the EBS disk to be attached (in GB)"
+  type        = number
+  default     = 10  # Default disk size in GB
+}
