43a7ca24b6773b0229be1c7db9347fa60065b5e5
prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>
2025-03-14 22:20:22 +0530

    Update variable.tf
    Signed-off-by: prashanthgowdaN <39018026+prashanthgowdaN@users.noreply.github.com>

--- variable.tf
+++ variable.tf
@@ -1,7 +1,7 @@
-variable "aws_access_key" {}
+/* variable "aws_access_key" {}
 variable "aws_secret_key" {}
 variable "vm_names" {
   type    = list(string)
   default = ["web-server", "db-server", "cache-server"]  # We can use the Host Name and its only for single Image 
-}
+}*/
 
